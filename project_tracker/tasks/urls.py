348f35b7b07523713c7a15923b37fabea35282ea
movereCrus <vance200045@gmail.com>
2024-04-30 22:42:26 +0300

    Исправления tasks. В итоге не сделал две последние лекции

--- project_tracker/tasks/urls.py
+++ project_tracker/tasks/urls.py
@@ -5,7 +5,10 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('projects/', views.projects_list, name='projects_list'),
     path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
-    path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail')
+    path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('projects/new/', views.create_project, name='create_project'),
+    path('projects/<int:project_id>/add_task/', views.add_task_to_project, name='add_task_to_project')
     # path('another/', views.another_page, name='another_page')
 ]
--- project_tracker/tasks/views.py
+++ project_tracker/tasks/views.py
@@ -1,31 +1,47 @@
 from django.http import HttpResponse
 from django.urls import reverse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
+from tasks.forms import ProjectForm, TaskForm
 from tasks.models import Project, Task
-
-# def index(request):
-#     another_page_url = reverse('tasks:another_page')
-#     quality_control_url = reverse('quality_control:index')
-#     html = f"""<h1>Страница приложения tasks</h1>
-#         <a href='{another_page_url}'>Перейти на другую страницу</a>
-#         <a href='{quality_control_url}'>Перейти на главную страницу контроля качеством"""
-#     return HttpResponse(html)
 
 def index(request):
     return render(request, 'tasks/index.html')
 
 def projects_list(request):
     projects = Project.objects.all()
-    return render(request, 'tasks/projects_list.html', {'project_list': projects})
+    return render(request, 'tasks/projects_list.html', {'projects_list': projects})
 
 def project_detail(request, project_id):
-    project = get_object_or_404(Task, project_id=project_id)
-    return render(request, 'tasks/task_detail.html', {'project': project})
+    project = get_object_or_404(Project, id=project_id)
+    return render(request, 'tasks/project_detail.html', {'project': project})
 
 def task_detail(request, task_id, project_id):
-    task = get_object_or_404(Task, task_id=task_id, project_id=project_id)
+    task = get_object_or_404(Task, id=task_id, project_id=project_id)
     return render(request, 'tasks/task_detail.html', {'task': task})
 
 def another_page(request):
     return HttpResponse('Это другая страница приложения tasks')
+
+def create_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:projects_list')
+    else:
+        form = ProjectForm()
+    return render(request, 'tasks/project_create.html', {'form': form})
+
+def add_task_to_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.project = project
+            task.save()
+            return redirect('tasks:task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form, 'project': project})
